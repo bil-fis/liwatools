1e0a709c438214293a02f33e4e40ab1b14c79053
bil_fis <468840127@qq.com>
2026-08-21 15:02:03 +0800

    fix: 解决了报错找不到Prism的问题

--- src/tools/OcrTool.vue
+++ src/tools/OcrTool.vue
@@ -75,7 +75,7 @@ async function getEngine() {
     ocr = await PaddleOCR.create({
       lang: lang.value,
       ocrVersion: 'PP-OCRv5',
-      ortOptions: { backend: 'wasm', numThreads: 1, simd: true,wasmPaths:"https://cdn.staticfile.net/onnxruntime-web/1.22.0/" },
+      ortOptions: { backend: 'wasm', numThreads: 1, simd: true,wasmPaths:"https://cdn.staticfile.net/onnxruntime-web/1.27.0/" },
     })
     engineInfo.value = 'PP-OCRv5'
     status.value = 'OCR 引擎就绪'
